03b2430d099c173e59e7640215ec19ca993fe377
osvaldsoza <osvald.soza@gmail.com>
2023-11-11 19:44:01 -0300

    Add new annotation

--- src/main/java/com/github/osvaldsoza/foodapi/domain/model/Restaurante.java
+++ src/main/java/com/github/osvaldsoza/foodapi/domain/model/Restaurante.java
@@ -1,8 +1,7 @@
-package com.github.osvaldsoza.foodapi.model;
+package com.github.osvaldsoza.foodapi.domain.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,10 +17,14 @@ import java.math.BigDecimal;
 public class Restaurante {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
     private String nome;
 
     private BigDecimal taxaFrete;
+
+    @ManyToOne
+    private Cozinha cozinha;
 }
