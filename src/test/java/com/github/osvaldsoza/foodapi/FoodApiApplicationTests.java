5daee5e99b1ebccc43b792d19ba844934a05b259
osvaldsoza <osvald.soza@gmail.com>
2023-11-11 14:56:11 -0300

    Add dependecies

--- src/test/java/com/github/osvaldsoza/foodapi/FoodApiApplicationTests.java
+++ src/test/java/com/github/osvaldsoza/foodapi/FoodApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FoodApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
